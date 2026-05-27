ff7a0868c6fbe42180aa990bb9af0b1b34336365
Abhishek <abhisheksengar729@gmail.com>
2026-05-27 23:23:04 +0530

    updated aout ,add accordion

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require("express");
 
 const app = express();
 
+
+
 app.set("view engine", "pug");
 app.set("views", "./views");
 
 
 app.use(express.static("public"));
 
+
+
+
+
 app.get("/", (req, res) => {
   res.render("index",{
     name:"mentorshelp",
     website:"https//:mentorshelp.com",
-    user:{name:"abhishek",roll:"1"}
+    user:{name:"abhishek",roll:"1"},
+    title: "Home",
+    page:"home"
   });
 });
 
@@ -22,6 +30,8 @@ app.get("/about", (req,res)=>{
         title: "About Page",
     company: "MentorsHelp",
     founder: "Krishna Patel",
+    // title: "About",
+    page:"about"
     });
 })
 
